d1d49238cab0e0d314f63f8fe040e7c1445bd14b
garima-pachori <96126366+garima-pachori@users.noreply.github.com>
2023-09-22 22:49:33 +0530

    .

--- airbnb/src/App.jsx
+++ airbnb/src/App.jsx
@@ -1,12 +1,23 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Card from "./components/card"
+import katie from "./images/katie-zaferes.png"
+
 
 export default function App() {
+            // <Hero />
     return (
         <div>
-            <Navbar/>
-            <Hero/>
+            <Navbar />
+            <Card 
+                img={katie}
+                rating="5.0"
+                reviewCount={6}
+                country="USA"
+                title="Life Lessons with Katie Zaferes"
+                price={136}
+            />
         </div>
     )
 }
---
+++ airbnb/src/components/card.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "../images/katie-zaferes.png"
+
+export default function Card(props) {
+    return (
+        <div className="card">
+            <img src={imagePath} className="card--image" />
+            <div className="card--stats">
+                <img src="../images/star.png" className="card--star" />
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray">{props.country}</span>
+            </div>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
+        </div>
+    )
+}
